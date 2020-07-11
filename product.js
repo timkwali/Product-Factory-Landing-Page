1e6dc714426a852414ae938ad71b0eb058deaf5e
Timring Timkwali <timringperez@gmail.com>
2020-07-11 20:20:58 +0100

    add welcome

--- product.js
+++ product.js
@@ -1,5 +1,7 @@
 const HAMBURGER = document.querySelector(".hamburger");
+const SIDENAV = document.querySelector(".side-nav");
 
 HAMBURGER.addEventListener("click", (e) => {
-    HAMBURGER.classList.toggle("hamburgerCancel")
+    HAMBURGER.classList.toggle("hamburgerCancel");
+    SIDENAV.classList.toggle("hideSideNav");
 })
